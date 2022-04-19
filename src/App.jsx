4892bc03e82662caea460080e309582fd38a8c1e
akshatmittal61 <akshatmittal2506@gmail.com>
2022-04-20 03:05:01 +0530

    added Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Navigation from "./components/Navigation/Navigation";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import GlobalContext from "./Context/GlobalContext";
 
 const App = () => {
 	AOS.init();
+	const { setOpenNav } = useContext(GlobalContext);
+	const location = useLocation();
+	useEffect(() => {
+		setOpenNav(false);
+	}, [location.pathname, setOpenNav]);
 	return (
 		<>
 			<Navigation />
-			<Home />
-			<Footer />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+			{location.pathname !== "/login" &&
+				location.pathname !== "/register" && <Footer />}
 		</>
 	);
 };
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { ArrowLeftCircle, AtSign, Key } from "react-feather";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../components/Button/Button";
+import "./login.css";
+
+const Login = () => {
+	const navigate = useNavigate();
+	const [user, setUser] = useState({
+		username: "",
+		password: "",
+	});
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setUser({
+			...user,
+			[name]: value,
+		});
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(user);
+		setUser({
+			username: "",
+			password: "",
+		});
+	};
+	return (
+		<section className="login">
+			<Button
+				text={
+					<>
+						<ArrowLeftCircle /> Go Back
+					</>
+				}
+				onClick={() => navigate(-1)}
+				className="login-back"
+			/>
+			<div className="login-content">
+				<div className="login-head">Welcome Back...</div>
+				<div className="login-box">
+					<form className="login-form" onSubmit={handleSubmit}>
+						<div className="login-form-group">
+							<label>
+								<AtSign />
+							</label>
+							<input
+								type="text"
+								name="username"
+								placeholder="Username"
+								value={user.username}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="login-form-group">
+							<label>
+								<Key />
+							</label>
+							<input
+								type="password"
+								name="password"
+								placeholder="Password"
+								value={user.password}
+								onChange={handleChange}
+							/>
+						</div>
+						<div className="login-form-group">
+							<Button text="Login" type="submit" />
+						</div>
+					</form>
+					<div className="login-foot">
+						<span>Don't Have an account yet? </span>
+						<Link to="/register">Sign Up</Link>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Login;
