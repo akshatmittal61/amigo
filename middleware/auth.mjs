17e06f16401c1a52799d90e056cfac15198c88a9
Akshat Mittal <akshatmittal2506@gmail.com>
2022-05-05 00:54:12 +0530

    created auth middleware

---
+++ middleware/auth.mjs
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+	const token = req.header("x-auth-token");
+	if (!token)
+		return res
+			.status(401)
+			.json({ message: "No Token. Authorization denied" });
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded.user;
+		next();
+	} catch (err) {
+		console.error(err);
+		res.status(401).json({ message: "Token is not valid" });
+	}
+};
+
+export default auth;
