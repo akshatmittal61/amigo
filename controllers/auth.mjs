7c1cc7853e0d0e022f48bc2a371253cca2ac1d10
Akshat Mittal <akshatmittal2506@gmail.com>
2022-05-05 13:01:15 +0530

    completed login route

--- controllers/auth.mjs
+++ controllers/auth.mjs
@@ -40,4 +40,38 @@ const register = async (req, res) => {
 	}
 };
 
-export { register };
+const login = async (req, res) => {
+	const { username, password } = req.body;
+	if (!username || !password)
+		return res
+			.status(400)
+			.json({ message: "Username and Password are required" });
+	try {
+		let user = await User.findOne({ username });
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch)
+			return res.status(400).json({ message: "Invalid credentials" });
+		const payload = {
+			user: {
+				id: user.id,
+			},
+		};
+		jwt.sign(
+			payload,
+			process.env.JWT_SECRET,
+			{ expiresIn: 360000 },
+			(err, token) => {
+				if (err) throw err;
+				res.status(200).json({ token, message: "Login successful" });
+			}
+		);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ message: "Server Error" });
+	}
+};
+
+export { register, login };
